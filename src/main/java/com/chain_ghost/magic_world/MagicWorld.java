3ae409cbe43d29889be6e88289e81b7f923e19ae
ChainGhost <19172961309@163.com>
2024-10-02 22:13:39 +0800

    ChainGhost

--- src/main/java/com/chain_ghost/magic_world/MagicWorld.java
+++ src/main/java/com/chain_ghost/magic_world/MagicWorld.java
@@ -6,11 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MagicWorld implements ModInitializer {
-	public static final String MOD_ID = "magic_world";
-
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+
+	public static final String MOD_ID = "magic_world";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
---
+++ src/main/java/com/chain_ghost/magic_world/MagicWorldClient.java
@@ -0,0 +1,10 @@
+package com.chain_ghost.magic_world;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicWorldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
